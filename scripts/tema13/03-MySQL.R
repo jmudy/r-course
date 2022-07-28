362484948510e413c1e5362cfb5f6d4ad03e947e
Jesús Mudarra Luján <62428273+jmudy@users.noreply.github.com>
2022-07-28 20:25:55 +0200

    lib añadida

--- scripts/tema13/03-MySQL.R
+++ scripts/tema13/03-MySQL.R
@@ -2,19 +2,14 @@
 # Directorio de trabajo
 setwd("~/repos/r-course/scripts/tema13")
 
-install.packages("RODBC") # Instalar con install.packages('RODBC')
-install.packages("RJDBC") # Instalar con install.packages('RJDBC')
-install.packages("RMySQL") # Instalar con install.packages('RMySQL')
-
-library(RODBC)
+library(RODBC) # Instalar con install.packages('RODBC')
 odbcDataSources()
 
-con <- odbcConnect("myodbc",
-                   uid = "jb",
-                   pwd = "1234")
+con <- odbcConnect("MySQL",
+                   uid = "jmudy",
+                   pwd = "v@lencia92")
 
 custData <- sqlQuery(con, "select rating from RecommendationSystem.Rating limit 5;")
-
 custData
 
 customers <- c("Juan Gabriel", "Ricardo", "María")
@@ -27,13 +22,13 @@ sqlSave(con, orders, "RecommendationSystem.Orders", append = F)
 df <- sqlQuery(con, "select * from RecommendationSystem.Orders")
 df
 
-library(RMySQL)
+library(RMySQL) # Instalar con install.packages('RMySQL')
 con <- dbConnect(MySQL(),
                  dbname = "RecommendationSystem",
                  host = "127.0.0.1", 
                  port = 3306, 
-                 username = "jb",
-                 password = "1234")
+                 username = "jmudy",
+                 password = "v@lencia92")
 
 ac <- dbReadTable(con, "Accomodation")
 head(ac)
@@ -59,9 +54,11 @@ dbClearResult(rs)
 dbDisconnect(con)
 dbListConnections(dbDriver("MySQL"))
 
-library(RJDBC)
+library(RJDBC) # Instalar con install.packages('RJDBC')
+# Descargar https://downloads.mysql.com/archives/get/p/3/file/mysql-connector-java-8.0.29.tar.gz
+# Y copiarlo en en carpeta lib/
 driver <- JDBC("com.mysql.jdbc.Driver", 
-               classPath = "../lib/mysql-connector-java-5.1.45/mysql-connector-java-5.1.45-bin.jar", "'")
+               classPath = "lib/mysql-connector-java-8.0.29/mysql-connector-java-8.0.29.jar", "'")
 con <- dbConnect(driver, "jdbc:mysql://127.0.0.1:3306/RecommendationSystem", 
                  "jb", "1234", useSSL = F)
 
